3f6569d00bf8802b12058b63bdb66aabb6a373bb
mateusz-osetek <mateusz.osetek@eengine.pl>
2021-01-27 23:55:33 +0100

    Initial commit

--- src/Core/Domain/Model/AggregateRoot.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Core\Domain\Model;
-
-interface AggregateRoot extends Entity
-{
-}
--- src/Core/Domain/Repository/DocumentRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Core\Domain\Repository;
-
-class DocumentRepository extends \Doctrine\ODM\MongoDB\Repository\DocumentRepository
-{
-}
